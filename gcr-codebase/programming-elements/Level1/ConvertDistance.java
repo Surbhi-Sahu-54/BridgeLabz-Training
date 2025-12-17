a6ad8c4c5f8adaada7cf3bfd4de6ce57d39b218d
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-17 22:57:19 +0530

    [Surbhi] Convert distance from kilometers into miles

---
+++ gcr-codebase/programming-elements/Level1/ConvertDistance.java
@@ -0,0 +1,12 @@
+// Creating program to convert the distance from kilometre to miles 
+
+//creating  ConvertDistance class to convert the distance from kilometers to miles 
+public class ConvertDistance{
+      public static void main(String[] args){
+       
+         double kilometer = 10.8;   // the distance in kilometers 
+         double distance = kilometer *1.6; // distance in miles 
+      
+     System.out.println("The distance "+ kilometer +"km in miles is "+ distance);
+}
+}
