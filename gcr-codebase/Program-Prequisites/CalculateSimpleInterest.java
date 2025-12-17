94083f41aeb80db629eb083cdcc80cd422130c5b
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-17 22:43:37 +0530

    [Surbhi] Calculate Simple Interest

---
+++ gcr-codebase/Program-Prequisites/CalculateSimpleInterest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CalculateSimpleInterest{
+     public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+           double Principal = sc.nextDouble();
+           double Rate = sc.nextDouble();
+           double Time = sc.nextDouble();
+
+        double Simple_Interest = Principal*Rate*Time /100;
+
+        System.out.println(Simple_Interest);
+}
+}
+
+
