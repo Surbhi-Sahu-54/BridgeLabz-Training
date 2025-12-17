f9d08da23f9093b14b007d365504fef985ac5d11
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-17 16:13:10 +0530

    [Surbhi] Added Harry's age

---
+++ gcr-codebase/programming-elements/Level1/HarryAge.java
@@ -0,0 +1,16 @@
+
+// creating a program to calculate the age of Harry 
+
+//creating class with name HarryAge 
+public class HarryAge{
+        public static void main(String[] args){
+          int birth_year = 2000;  //2000 is the birth year 
+          int current_year = 2024;  // 2024 is the current year 
+
+        int age = current_year - birth_year; 
+               //calculate age by substract birth year from current year
+
+       System.out.println("Harry's age in 2024 is " + age); // Age result 
+}
+}
+    
