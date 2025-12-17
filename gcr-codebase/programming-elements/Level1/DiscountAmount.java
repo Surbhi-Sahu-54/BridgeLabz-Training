10b3f9b5bfcb6dc5f4af4c6c929da9f687a240e5
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-17 23:00:55 +0530

    [Surbhi] Calculate discounted amount of course fee

---
+++ gcr-codebase/programming-elements/Level1/DiscountAmount.java
@@ -0,0 +1,14 @@
+// Creating a program to calculate the discounted amount and discounted price of the course 
+
+public class DiscountAmount {
+  public static void main(String[] args){
+     int fee = 125000;
+     int discount_percent =10;
+
+    int discount= fee * distance_percent /100 ;
+    int final_fee = fee - discount;
+    
+     System.out.println(" The discount amount is INR " + discount +" and the final  discounted fees is INR " + final fee );
+}
+}
+  
