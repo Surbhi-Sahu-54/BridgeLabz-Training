09098d5e98874d88ee00c8726d7a0582f1f4e992
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-17 22:45:19 +0530

    [Surbhi] Convert kilometer to miles

---
+++ gcr-codebase/Program-Prequisites/ConvertKilometersToMiles.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ 
+public class ConvertKilometersToMiles{
+      public static void main(String[] args){
+           Scanner sc= new Scanner(System.in);
+              double distance = sc.nextDouble();
+             
+             double miles= distance*0.621371;
+ 
+       System.out.println(miles);
+}
+}
