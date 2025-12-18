283e808a36fab0acc5b68741f4b3d7afb29e551d
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-18 23:12:27 +0530

    [Surbhi] Sum of natural numbers

---
+++ gcr-codebase/control-flow/Level-1/NaturalNumberSum.java
@@ -0,0 +1,20 @@
+// create a class to  to check the natural number and calculate the sum of n natural numbers by using while loop
+
+import java.util.Scanner;
+
+public class NaturalNumberSum{
+     public static void main(String[] args){
+        
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter number");
+        int number = input.nextInt();
+       
+        // check if the no. is natural or not  
+        while(number>=0)
+        int sum = number *(number +1)/2;
+
+        System.out.println("the sum of" + number + "natural numbers is " + sum);
+        }
+        input.close();
+}
+}
