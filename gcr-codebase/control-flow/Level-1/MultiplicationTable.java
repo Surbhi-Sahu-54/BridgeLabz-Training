b3ab489b7642f23bc4c39ad53b8d20436d1d975d
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-19 10:49:18 +0530

    [Surbhi] Multiplication table

---
+++ gcr-codebase/control-flow/Level-1/MultiplicationTable.java
@@ -0,0 +1,19 @@
+// calculate the multiplication table of a number from 6 to9
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        // Print multiplication table from 6 to 9
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+
+        input.close();
+    }
+}
