54158cb635ef09b67aa1b910f06a653afe22b665
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-19 15:31:29 +0530

    [Surbhi]: find side of a square

---
+++ gcr-codebase/programming-elements/Level1/SquareSide.java
@@ -0,0 +1,24 @@
+// creating a program to find the side of a square 
+
+import java.util.Scanner;
+ public class SquareSide{
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+     System.out.println(" Enter the peimeter of square");
+      double perimeter= sc.nextDouble();
+     double side = perimeter/4;
+  
+   System.out.println(" The length of the side is" + side + " whose perimeter is " + perimeter);
+}
+}
+
+
+
+
+      
+
+
+
+
+
+
