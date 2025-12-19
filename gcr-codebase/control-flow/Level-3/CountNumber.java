f36db065dbffcab840f4946ecf1e3959eec6f2af
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-19 15:34:57 +0530

    [Surbhi]: Count numbers

---
+++ gcr-codebase/control-flow/Level-3/CountNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CountNumber{
+        public static void main(String[] args){
+         Scanner input = new Scanner(System.in);
+         System.out.println("Enter the number");
+         int number = input.nextInt();
+         
+         int count =0;
+         while(number !=0){
+          number =number/10;
+         count ++;
+   
+       System.out.println("The number of digit "+ count );
+}
+}
+}
