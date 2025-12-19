2dce56ebb447ff2db3996a9f24334deade6921d8
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-19 15:21:06 +0530

    [Surbhi]: Sam marks

---
+++ gcr-codebase/programming-elements/Level1/SamMarks.java
@@ -0,0 +1,14 @@
+//Creating a program to calculate the average percentage mark in PCM by Sam
+
+//creating a class with name SamMarks 
+public class SamMarks {
+       public static void main(String[] args){
+             int maths_mark =94;  // Sam's maths Marks 
+             int physics_mark = 95;  //  Sam's physics marks
+             int chemistry_mark = 96; // Sam's chemistry marks 
+
+        int average_mark= (maths_mark +physics_mark + chemistry_mark)/3;
+               //  resultant average marks of Sam
+        System.out.println("Sam's average mark in PCM is " + average_mark);  // Final output 
+}
+}
