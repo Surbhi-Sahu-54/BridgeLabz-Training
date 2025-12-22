e3290a927f6c98845eb87f48b02320177cf72664
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-22 11:04:18 +0530

    [Surbhi]: Check voting eligibility file added

---
+++ gcr-codebase/arrays/Level-1/VotingEligibility.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int[] ages = new int [10];
+        for (int i=0; i<ages.length; i++){
+        System.out.println("Enter age of student " + (i+1) + );
+        ages [i]= input.nextInt();
+        }
+
+        for (int i==0;i<ages.length; i++){
+        if (ages[i]<0){
+        System.out.println("Invalid age");
+        }else if(ages [i] >=18)
+        {
+        System.out.println(" The student with the age" + ages[i] + "can vote");
+        } else{
+        System.out.println("The student with the age" + age[i] + "cannot vote");
+        }
+       }
+      }
+    }
+
