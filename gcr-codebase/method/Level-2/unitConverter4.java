887351758a199d0d6916a0b976b96b0b126ef0a3
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-23 11:29:52 +0530

    [Surbhi]: unit cnverter file added

---
+++ gcr-codebase/method/Level-2/unitConverter4.java
@@ -0,0 +1,42 @@
+public class UnitConvertor4 {
+
+    // Method to convert Fahrenheit to Celsius
+    public static double convertFarhenheitToCelsius(double farhenheit) {
+        double farhenheit2celsius = (farhenheit - 32) * 5 / 9;
+        return farhenheit2celsius;
+    }
+
+    // Method to convert Celsius to Fahrenheit
+    public static double convertCelsiusToFarhenheit(double celsius) {
+        double celsius2farhenheit = (celsius * 9 / 5) + 32;
+        return celsius2farhenheit;
+    }
+
+    // Method to convert pounds to kilograms
+    public static double convertPoundsToKilograms(double pounds) {
+        double pounds2kilograms = 0.453592;
+        double kilograms = pounds * pounds2kilograms;
+        return kilograms;
+    }
+
+    // Method to convert kilograms to pounds
+    public static double convertKilogramsToPounds(double kilograms) {
+        double kilograms2pounds = 2.20462;
+        double pounds = kilograms * kilograms2pounds;
+        return pounds;
+    }
+
+    // Method to convert gallons to liters
+    public static double convertGallonsToLiters(double gallons) {
+        double gallons2liters = 3.78541;
+        double liters = gallons * gallons2liters;
+        return liters;
+    }
+
+    // Method to convert liters to gallons
+    public static double convertLitersToGallons(double liters) {
+        double liters2gallons = 0.264172;
+        double gallons = liters * liters2gallons;
+        return gallons;
+    }
+}
