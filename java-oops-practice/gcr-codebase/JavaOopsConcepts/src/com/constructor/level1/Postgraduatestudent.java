dda0afb44a88def9d31c8a4ca978052ef2cbaea2
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2025-12-28 21:47:09 +0530

    [Surbhi]: subclass postgraduate student added

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Postgraduatestudent.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Postgraduatestudent.java
@@ -1,5 +1,18 @@
 package com.constructor.level1;
 
-public class Postgraduatestudent {
 
+public class PostgraduateStudent extends Student{
+	
+	public PostgraduateStudent(String rollNumber, String name,double cgpa) {
+
+// calling Student class constructor
+super("0111CS221207", "Priya", 8.5);
+	}
+	
+	public void showPostGraduateDetails() {
+		//accessing the protected member of parent class
+		System.out.println("PostGraduate Student Name is: "+ name);	
+		super.display();
+	}
+	
 }
