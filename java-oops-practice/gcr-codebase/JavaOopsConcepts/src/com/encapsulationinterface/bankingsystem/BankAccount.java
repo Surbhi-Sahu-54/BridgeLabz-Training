59d3a7c6ac6bc0e441768a4937bab6336e92223c
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-02 23:33:00 +0530

    [Surbhi]: Added BankAccount class of Banking System program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationinterface/bankingsystem/BankAccount.java
@@ -0,0 +1,53 @@
+package com.encapsulationinterface.bankingsystem;
+
+public abstract class BankAccount implements Loanable {
+
+    private String accountNumber;
+    private String holderName;
+    protected double balance;
+
+    public BankAccount(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // Encapsulation
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Concrete methods
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: ₹" + amount);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: ₹" + amount);
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    // Abstract method
+    public abstract double calculateInterest();
+
+    public void displayAccount() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Holder Name: " + holderName);
+        System.out.println("Balance: ₹" + balance);
+    }
+}
