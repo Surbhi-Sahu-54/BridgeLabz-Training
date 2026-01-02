21bf98a190b0dbe8411ecd69248d891dfea6b6ea
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-02 11:08:25 +0530

    [Surbhi]: Added main class of animal hierarchy

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/animalhierarchy/AnimalHierarchyMain.java
@@ -0,0 +1,14 @@
+package com.inheritance.animalhierarchy;
+
+public class AnimalHierarchyMain {
+    public static void main(String[] args) {
+
+        Animal a1 = new Dog("Buddy", 3);
+        Animal a2 = new Cat("Kitty", 2);
+        Animal a3 = new Bird("Tweety", 1);
+
+        a1.makeSound();  // Dog barks
+        a2.makeSound();  // Cat meows
+        a3.makeSound();  // Bird chirps
+    }
+}
