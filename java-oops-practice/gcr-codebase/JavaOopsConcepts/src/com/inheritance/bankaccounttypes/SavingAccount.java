5c100f2ad337a6f2b2d96cf51312597342b25a4a
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-02 11:25:16 +0530

    [Surbhi]: Added Saving Account class of Bank Account Type program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/bankaccounttypes/SavingAccount.java
@@ -0,0 +1,16 @@
+package com.inheritance.bankaccounttypes;
+
+	class SavingsAccount extends BankAccount {
+	    private double interestRate;
+
+	    SavingsAccount(String accountNumber, double balance, double interestRate) {
+	        super(accountNumber, balance);
+	        this.interestRate = interestRate;
+	    }
+
+	    void displayAccountType() {
+	        System.out.println("Account Type   : Savings Account");
+	        System.out.println("Interest Rate : " + interestRate + "%");
+	    }
+
+}
