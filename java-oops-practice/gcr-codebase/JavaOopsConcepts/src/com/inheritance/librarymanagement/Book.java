d573fe38deefd1aa570433fa4dd5d1c69b7a5ea1
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-02 11:49:52 +0530

    [Surbhi]: Added Book class of Library management program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/librarymanagement/Book.java
@@ -0,0 +1,18 @@
+package com.inheritance.librarymanagement;
+
+public class Book {
+	    protected String title;
+	    protected int publicationYear;
+
+	    // Constructor
+	    Book(String title, int publicationYear) {
+	        this.title = title;
+	        this.publicationYear = publicationYear;
+	    }
+
+	    void displayInfo() {
+	        System.out.println("Book Title       : " + title);
+	        System.out.println("Publication Year : " + publicationYear);
+	    }
+	}
+
