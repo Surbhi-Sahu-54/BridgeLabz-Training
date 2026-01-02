0dc1e88fe75a9d37e44e8ff0c3e11a68851100f9
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-02 11:07:15 +0530

    [Surbhi]: Added Animal class of Animal Hierarchy file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/animalhierarchy/Animal.java
@@ -0,0 +1,17 @@
+package com.inheritance.animalhierarchy;
+
+public class Animal {
+	  String name;
+	    int age;
+
+	    // Constructor
+	    Animal(String name, int age) {
+	        this.name = name;
+	        this.age = age;
+	    }
+
+	    // Method to be overridden
+	    void makeSound() {
+	        System.out.println("Animal makes a sound");
+	    }
+}
