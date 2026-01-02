cce4a2406652e7ed6468a18c1dd2101ad924cbca
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-03 00:02:53 +0530

    [Surbhi]:Added Vehicle class of Ride Hailing Application

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationinterface/ridehailingapplication/Vehicle.java
@@ -0,0 +1,47 @@
+package com.encapsulationinterface.ridehailingapplication;
+
+public abstract class Vehicle implements GPS {
+
+    private String vehicleId;
+    private String driverName;
+    protected double ratePerKm;
+
+    private String currentLocation;
+
+    public Vehicle(String vehicleId, String driverName, double ratePerKm) {
+        this.vehicleId = vehicleId;
+        this.driverName = driverName;
+        this.ratePerKm = ratePerKm;
+        this.currentLocation = "Unknown";
+    }
+
+    // Abstract method
+    public abstract double calculateFare(double distance);
+
+    // Concrete method
+    public void getVehicleDetails() {
+        System.out.println("Vehicle ID: " + vehicleId);
+        System.out.println("Driver Name: " + driverName);
+        System.out.println("Rate per Km: ₹" + ratePerKm);
+        System.out.println("Current Location: " + currentLocation);
+    }
+
+    // Encapsulation
+    protected String getVehicleId() {
+        return vehicleId;
+    }
+
+    protected String getDriverName() {
+        return driverName;
+    }
+
+    @Override
+    public String getCurrentLocation() {
+        return currentLocation;
+    }
+
+    @Override
+    public void updateLocation(String newLocation) {
+        this.currentLocation = newLocation;
+    }
+}
