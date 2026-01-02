368b6d468df48d8500e54288cf3a319e912dd004
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-02 11:51:42 +0530

    [Surbhi]: Added Main class of Library Management

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/librarymanagement/Librarymanagementmain.java
@@ -0,0 +1,15 @@
+package com.inheritance.librarymanagement;
+
+public class Librarymanagementmain {
+	    public static void main(String[] args) {
+
+	        Author book1 = new Author(
+	                "Java Programming",
+	                2023,
+	                "James Gosling",
+	                "Creator of Java programming language"
+	        );
+
+	        book1.displayInfo();
+	    }
+}
