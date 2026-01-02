89d79ddca831268162d8579ac2475453515bac4a
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-03 01:02:00 +0530

    [Surhi]: Added Fooditem class of e-commerce Platform Online Food Delivery System

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationinterface/ecommerceplatformonlinefooddeliverysystem/FoodItem.java
@@ -0,0 +1,36 @@
+package com.encapsulationinterface.ecommerceplatformonlinefooddeliverysystem;
+public abstract class FoodItem implements Discountable {
+
+    private String itemName;
+    private double price;
+    private int quantity;
+
+    public FoodItem(String itemName, double price, int quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Encapsulation (no direct modification)
+    public String getItemName() {
+        return itemName;
+    }
+
+    protected double getPrice() {
+        return price;
+    }
+
+    protected int getQuantity() {
+        return quantity;
+    }
+
+    // Abstract method
+    public abstract double calculateTotalPrice();
+
+    // Concrete method
+    public void getItemDetails() {
+        System.out.println("Item: " + itemName);
+        System.out.println("Price: ₹" + price);
+        System.out.println("Quantity: " + quantity);
+    }
+}
