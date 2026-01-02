6324c495fdef7cf199e9cb8df0280bc074060653
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-02 11:13:04 +0530

    [Surbhi]: Added Dog class of Aimal hierarchy

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/animalhierarchy/Dog.java
@@ -0,0 +1,13 @@
+package com.inheritance.animalhierarchy;
+
+class Dog extends Animal {
+
+    Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
