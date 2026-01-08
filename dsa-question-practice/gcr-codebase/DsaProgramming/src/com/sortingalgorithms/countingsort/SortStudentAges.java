f86595452fbda57e3173f1f6a6f15c3b88392f4a
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-08 19:12:01 +0530

    [Surbhi]:Added Sort Student Ages program of Counting Sort

---
+++ dsa-question-practice/gcr-codebase/DsaProgramming/src/com/sortingalgorithms/countingsort/SortStudentAges.java
@@ -0,0 +1,59 @@
+package com.sortingalgorithms.countingsort;
+import java.util.Scanner;
+
+public class SortStudentAges {
+
+	static void countingSort(int[] ages) {
+		int minAge = 10;
+		int maxAge = 18;
+		int range = maxAge - minAge + 1;
+
+		int[] count = new int[range];
+		int[] output = new int[ages.length];
+
+		// Storing frequency of each age
+		for (int age : ages) {
+			count[age - minAge]++;
+		}
+
+		// Calculating cumulative count
+		for (int i = 1; i < range; i++) {
+			count[i] += count[i - 1];
+		}
+
+		// storing elements in correct position
+		for (int i = ages.length - 1; i >= 0; i--) {
+			int age = ages[i];
+			output[count[age - minAge] - 1] = age;
+			count[age - minAge]--;
+		}
+
+		// Copy the element back to original array
+		for (int i = 0; i < ages.length; i++) {
+			ages[i] = output[i];
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter number of students: ");
+		int n = input.nextInt();
+
+		int[] ages = new int[n];
+
+		System.out.println("Enter student ages (10 to 18):");
+		for (int i = 0; i < n; i++) {
+			ages[i] = input.nextInt();
+		}
+
+		countingSort(ages);
+
+		System.out.println("\nSorted student ages are: ");
+		for (int age : ages) {
+			System.out.print(age + " ");
+		}
+
+		input.close();
+	}
+}
