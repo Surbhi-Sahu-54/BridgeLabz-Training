5c80688e9beded7dc3385d7b09a4ed4dd50a67a9
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-12 14:15:30 +0530

    [Surbhi]:Added Hotel class of Tour Mate Travel itinerary planner program

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/tourmatetravelitineraryplanner/Hotel.java
@@ -0,0 +1,13 @@
+package tourmatetravelitineraryplanner;
+
+public class Hotel {
+	    private double cost;
+
+	    public Hotel(double cost) {
+	        this.cost = cost;
+	    }
+
+	    public double getCost() {
+	        return cost;
+	    }
+}
