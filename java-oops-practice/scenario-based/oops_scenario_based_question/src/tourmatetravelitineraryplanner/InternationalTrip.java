6a7db545c89da94a4840e4af5f1b90909c496e89
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-12 14:20:28 +0530

    [Surbhi]:Added InternationalTrip class of  Tour Mate Travel itinerary planner program

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/tourmatetravelitineraryplanner/InternationalTrip.java
@@ -0,0 +1,15 @@
+package tourmatetravelitineraryplanner;
+
+public class InternationalTrip extends Trip {
+
+	    public InternationalTrip(String destination, int duration,
+	                             Transport transport, Hotel hotel, Activity activity) {
+	        super(destination, duration, transport, hotel, activity);
+	    }
+
+	    @Override
+	    public void book() {
+	        System.out.println("International Trip booked with passport & visa verification.");
+	    }
+
+}
