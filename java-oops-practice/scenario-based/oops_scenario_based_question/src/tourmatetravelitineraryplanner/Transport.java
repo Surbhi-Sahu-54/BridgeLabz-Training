08b06b98b0731d3d40d4b9045e3649562b9fdfb8
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-12 14:25:00 +0530

    [Surbhi]:Added Transport class of Tour Mate Travel Itenerary planner program

---
+++ java-oops-practice/scenario-based/oops_scenario_based_question/src/tourmatetravelitineraryplanner/Transport.java
@@ -0,0 +1,13 @@
+package tourmatetravelitineraryplanner;
+
+public class Transport {
+	    private double cost;   // hidden cost
+
+	    public Transport(double cost) {
+	        this.cost = cost;
+	    }
+
+	    public double getCost() {
+	        return cost;
+	    }
+}
