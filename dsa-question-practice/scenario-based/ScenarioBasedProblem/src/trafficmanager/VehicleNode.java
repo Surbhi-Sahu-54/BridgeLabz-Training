6a7400ce378d15335ae7310bdcd66d1f81c8cd7f
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-13 17:09:50 +0530

    [Surbhi]:Added VehicleNode class of TrafficManager – Roundabout Vehicle Flow

---
+++ dsa-question-practice/scenario-based/ScenarioBasedProblem/src/trafficmanager/VehicleNode.java
@@ -0,0 +1,11 @@
+package trafficmanager;
+
+public class VehicleNode {
+	    String vehicleNumber;
+	    VehicleNode next;
+
+	    public VehicleNode(String vehicleNumber) {
+	        this.vehicleNumber = vehicleNumber;
+	        this.next = null;
+	    }
+}
