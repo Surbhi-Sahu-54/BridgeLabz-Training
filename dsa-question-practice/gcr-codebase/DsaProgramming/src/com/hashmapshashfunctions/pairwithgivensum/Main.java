dc86de7b6327a2658c0996c0cc0afd4df583c60f
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-15 12:24:18 +0530

    [Surbhi]:Added Main class of Pair With Given Sum

---
+++ dsa-question-practice/gcr-codebase/DsaProgramming/src/com/hashmapshashfunctions/pairwithgivensum/Main.java
@@ -0,0 +1,11 @@
+package com.hashmapshashfunctions.pairwithgivensum;
+
+public class Main {
+	public static void main(String[] args) {
+
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+
+        pairwithgivensum.hasaPair(arr, target);
+    }
+}
