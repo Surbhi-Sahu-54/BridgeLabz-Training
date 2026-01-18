d5c8b1c46efdedec7cf4f7901b2796ff559df46a
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-18 12:38:18 +0530

    [Surbhi]:Added StackNode class of ExamProctor – Online Exam Review System

---
+++ dsa-question-practice/scenario-based/ScenarioBasedProblem/src/onlineexamreviewsystem/StackNode.java
@@ -0,0 +1,11 @@
+package onlineexamreviewsystem;
+
+public class StackNode {
+	   String question;
+	    StackNode next;
+
+	    StackNode(String question) {
+	        this.question = question;
+	        this.next = null;
+	    }
+}
