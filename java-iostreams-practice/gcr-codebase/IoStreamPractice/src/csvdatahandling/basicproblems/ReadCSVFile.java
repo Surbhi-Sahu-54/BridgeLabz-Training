625f2f590d49f72bd6d393d72380e430492d1a29
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-26 20:53:04 +0530

    [Surbhi]:Added ReadCSVFile program

---
+++ java-iostreams-practice/gcr-codebase/IoStreamPractice/src/csvdatahandling/basicproblems/ReadCSVFile.java
@@ -0,0 +1,25 @@
+package csvdatahandling.basicproblems;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadCSVFile {
+    public static void main(String[] args) {
+        String filePath = "D:\\Java Programming Workspace\\java-iostreams-practice\\gcr-codebase\\IoStreamPractice\\src\\csvdatahandling\\basicproblems\\ReadCSVFile" ;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line ;
+
+            br.readLine(); //to skip ID: ID, Name: Name, Age: Age, Marks: Marks
+
+            while ((line = br.readLine()) != null){
+                String[] columns = line.split(",");
+                System.out.println("ID: " + columns[0] + ", Name: " + columns[1] +
+                        ", Age: " + columns[2] + ", Marks: " + columns[3]);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
