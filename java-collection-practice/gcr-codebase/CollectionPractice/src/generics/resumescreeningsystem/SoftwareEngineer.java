9ac9dc9d115df8665a81f5f540524b43b466b77d
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-28 00:24:07 +0530

    [Surbhi]:Added SoftwareEngineer class of   AI-Driven Resume Screening System Program

---
+++ java-collection-practice/gcr-codebase/CollectionPractice/src/generics/resumescreeningsystem/SoftwareEngineer.java
@@ -0,0 +1,8 @@
+package generics.resumescreeningsystem;
+
+public class SoftwareEngineer extends JobRole {
+    public SoftwareEngineer() {
+        super("Software Engineer", 1, "Java");
+    }
+}
+
