2da99eed37853047fe4813ce7f48390459e3c5f3
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-01-30 22:56:41 +0530

    [Surbhi]:Added PetrolCar class of Smart Vehicle Dashboard

---
+++ java8-programming-practice/gcr-codebase/Java8Progrmming/src/com/functionalinterfaces/defaultmethodsininterface/smartvehicledashboard/PetrolCar.java
@@ -0,0 +1,8 @@
+package com.functionalinterfaces.defaultmethodsininterface.smartvehicledashboard;
+class PetrolCar implements VehicleDashboard {
+
+    @Override
+    public void displaySpeed(int speed) {
+        System.out.println("Petrol Car Speed: " + speed + " km/h");
+    }
+}
