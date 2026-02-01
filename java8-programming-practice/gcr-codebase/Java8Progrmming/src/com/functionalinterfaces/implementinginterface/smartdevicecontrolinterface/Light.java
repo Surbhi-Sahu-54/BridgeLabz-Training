7669d07448ec7b2adf3dbc027162c3b3be1bd7ef
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-02-01 22:16:20 +0530

    [Surbhi]:Added Light class of Smart Device Control Interface

---
+++ java8-programming-practice/gcr-codebase/Java8Progrmming/src/com/functionalinterfaces/implementinginterface/smartdevicecontrolinterface/Light.java
@@ -0,0 +1,14 @@
+package com.functionalinterfaces.implementinginterface.smartdevicecontrolinterface;
+
+class Light implements SmartDevice {
+
+    @Override
+    public void turnOn() {
+        System.out.println("Light is ON");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Light is OFF");
+    }
+}
