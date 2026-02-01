9706a58a5025e95899f09f050072425c8f35340f
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-02-01 22:35:05 +0530

    [Surbhi]:Added Unit Conversion Tool program

---
+++ java8-programming-practice/gcr-codebase/Java8Progrmming/src/com/functionalinterfaces/staticmethodsininterface/unitconversiontool/UnitConversionTool.java
@@ -0,0 +1,16 @@
+package com.functionalinterfaces.staticmethodsininterface.unitconversiontool;
+
+public class UnitConversionTool {
+
+    public static void main(String[] args) {
+
+        double km = 10;
+        double kg = 5;
+
+        double miles = UnitConverter.kmToMiles(km);
+        double lbs = UnitConverter.kgToLbs(kg);
+
+        System.out.println(km + " km = " + miles + " miles");
+        System.out.println(kg + " kg = " + lbs + " lbs");
+    }
+}
