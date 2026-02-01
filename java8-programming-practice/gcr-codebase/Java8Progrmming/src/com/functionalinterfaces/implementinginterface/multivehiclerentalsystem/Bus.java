2d247857260d780a85552eee85483bc62d18d35b
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-02-01 22:10:43 +0530

    [Surbhi]:Added Bus class of Multi Vehicle Rental System

---
+++ java8-programming-practice/gcr-codebase/Java8Progrmming/src/com/functionalinterfaces/implementinginterface/multivehiclerentalsystem/Bus.java
@@ -0,0 +1,13 @@
+package com.functionalinterfaces.implementinginterface.multivehiclerentalsystem;
+public class Bus implements RentalVehicle {
+
+    @Override
+    public void rent() {
+        System.out.println("Bus rented successfully");
+    }
+
+    @Override
+    public void returnVehicle() {
+        System.out.println("Bus returned");
+    }
+}
