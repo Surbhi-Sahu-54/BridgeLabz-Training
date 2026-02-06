09f9f01df1e68db8addaa91ff122a1360a562eb6
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-02-06 11:09:06 +0530

    [Surbhi]:Added Main class of pipedstreams program

---
+++ java-collection-practice/gcr-codebase/CollectionPractice/src/stream/pipedstreams/MainClass.java
@@ -0,0 +1,24 @@
+package stream.pipedstreams;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        try {
+            PipedOutputStream pos = new PipedOutputStream();
+            PipedInputStream pis = new PipedInputStream(pos); // connect streams
+
+            WriterThread writer = new WriterThread(pos);
+            ReaderThread reader = new ReaderThread(pis);
+
+            reader.start();
+            writer.start();
+
+        } catch (IOException e) {
+            System.out.println("Error connecting streams");
+        }
+    }
+}
