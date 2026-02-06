19317a1e90ceca567a05ac78b7418717ca1bf3e1
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-02-06 11:10:14 +0530

    [Surbhi]:Added Writer Thread program

---
+++ java-collection-practice/gcr-codebase/CollectionPractice/src/stream/pipedstreams/WriterThread.java
@@ -0,0 +1,23 @@
+package stream.pipedstreams;
+import java.io.IOException;
+import java.io.PipedOutputStream;
+
+public class WriterThread extends Thread {
+
+    private PipedOutputStream pos;
+
+    public WriterThread(PipedOutputStream pos) {
+        this.pos = pos;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String message = "Hello from Writer Thread";
+            pos.write(message.getBytes());
+            pos.close();
+        } catch (IOException e) {
+            System.out.println("Writer Thread Error");
+        }
+    }
+}
