b1d6b21c2104e16fefeef04e81e1e4e092b97757
Surbhi-Sahu-54 <surbhisahu3075@gmail.com>
2026-02-07 10:33:51 +0530

    [Surbhi]:Added Print Job class of Print Shop Job Scheduler

---
+++ java-design-pattern-practice/gcr-codebase/multithreadingconcept/src/multithreadingconceptcom/multithreading/printshopjobscheduler/PrintJob.java
@@ -0,0 +1,39 @@
+package multithreadingconceptcom.multithreading.printshopjobscheduler;
+
+public class PrintJob implements Runnable {
+
+    private String jobName;
+    private int pages;
+    private int priority;
+
+    public PrintJob(String jobName, int pages, int priority) {
+        this.jobName = jobName;
+        this.pages = pages;
+        this.priority = priority;
+    }
+
+    @Override
+    public void run() {
+
+        String level;
+
+        if (priority >= 7) {
+            level = "[High Priority]";
+        } else if (priority >= 5) {
+            level = "[Medium Priority]";
+        } else {
+            level = "[Low Priority]";
+        }
+
+        for (int i = 1; i <= pages; i++) {
+            System.out.println(level + " Printing " + jobName +
+                    " - Page " + i + " of " + pages);
+
+            try {
+                Thread.sleep(100); // 100ms per page
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
